bb7e96f46b8e62cd23f0da471cea15697a66604a
umeMusfera <umemusfira2@gmail.com>
2025-07-31 15:46:24 +0500

    Deploy using main branch

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,3 +13,21 @@ ReactDOM.render(
 );
 
 reportWebVitals();
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
+// import { Provider } from "react-redux";
+// import Store from "./redux/store";
+// import { BrowserRouter } from "react-router-dom";
+
+// ReactDOM.render(
+//   <Provider store={Store}>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </Provider>,
+//   document.getElementById("root")
+// );
+
+// reportWebVitals();
